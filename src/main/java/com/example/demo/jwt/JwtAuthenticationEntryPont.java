d65c7caa822c51765c5cfd5a815271589fcfdbde
kdj9878 <kdj9878@gmail.com>
2021-09-16 15:33:59 +0900

    compoment 어노테이션 추가

--- src/main/java/com/example/demo/jwt/JwtAuthenticationEntryPont.java
+++ src/main/java/com/example/demo/jwt/JwtAuthenticationEntryPont.java
@@ -8,9 +8,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 // 유효한 자격증명을 제공하지 않고 접근할 때
 // 401 Unauthorized 에러를 리턴하는 클래스
+@Component
 public class JwtAuthenticationEntryPont implements AuthenticationEntryPoint{
     
     @Override

f05be9a879c1ea843eff1f802072b54c65fee7fb
kdj9878 <kdj9878@gmail.com>
2021-09-16 15:29:54 +0900

    401에러 리턴하는 메소드 추가

---
+++ src/main/java/com/example/demo/jwt/JwtAuthenticationEntryPont.java
@@ -0,0 +1,21 @@
+package com.example.demo.jwt;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+// 유효한 자격증명을 제공하지 않고 접근할 때
+// 401 Unauthorized 에러를 리턴하는 클래스
+public class JwtAuthenticationEntryPont implements AuthenticationEntryPoint{
+    
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authenticationException)
+            throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
